30a92dc848407416667070e1c557ba921fdb9561
Dell2G96 <dell2g.1996@gmail.com>
2025-11-24 02:38:10 +0900

    몽타주 예약으로 최종 수정  - 클라에서 플레이 될때가 있고 안될때가 있음;

--- Source/GAS_Project/GAS/Abilities/GA_Combo.h
+++ Source/GAS_Project/GAS/Abilities/GA_Combo.h
@@ -1,58 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Abilities/GameplayAbility.h"
+#include "CGameplayAbility.h"
 #include "GA_Combo.generated.h"
 
-class UAnimMontage;
-
+/**
+ * 
+ */
 UCLASS()
-class GAS_PROJECT_API UGA_Combo : public UGameplayAbility
+class UGA_Combo : public UCGameplayAbility
 {
 	GENERATED_BODY()
-
+	
 public:
 	UGA_Combo();
-
-	// UGameplayAbility
-	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-		const FGameplayAbilityActorInfo* ActorInfo,
-		const FGameplayAbilityActivationInfo ActivationInfo,
-		const FGameplayEventData* TriggerEventData) override;
-
-protected:
-	// 태그 헬퍼
-	FGameplayTag GetComboChangeEventTag() const;
-	FGameplayTag GetComboChangeEventEndTag() const;
-
-	UFUNCTION(NetMulticast,Reliable)
-	void Multicast_JumpToSection(FName SectionName);
-
-	// 이벤트 핸들러들
-	UFUNCTION()
-	void OnComboWindowOpened(FGameplayEventData Data);
-
-	UFUNCTION()
-	void OnComboWindowEnded(FGameplayEventData Data);
-
-	UFUNCTION()
-	void OnInputPressed(float TimeWaited);
-
-	// 유틸
-	void SetupWaitInputTask();
-	class UAnimInstance* GetOwnerAnimInstance() const;
-
-protected:
-	UPROPERTY(EditDefaultsOnly, Category="Combo")
-	UAnimMontage* ComboMontage = nullptr;
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	static FGameplayTag GetComboChangeEventTag();
+	static FGameplayTag GetComboChangeEventEndTag();
 
 private:
-	// 콤보 창 상태
-	bool bComboWindowOpen = false;
+	void SetupWaitComboInputPress();
 
-	// 창이 열릴 때 태그에서 파싱한 "후보 섹션"
-	FName CandidateNextSection = NAME_None;
+	UFUNCTION()
+	void HandleInputPress(float TimeWaited);
 
-	// 실제로 입력이 들어와 확정된 다음 섹션
-	FName NextComboName = NAME_None;
+	void TryCommitCombo();
+	
+
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
+	UAnimMontage* ComboMontage;
+
+	UFUNCTION()
+	void ComboChangedEventReceived(FGameplayEventData Data);
+	
+	FName NextComboName;
 };
