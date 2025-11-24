0dfd141b439414a8aafd5545a09f8ec37d535fa8
Dell2G96 <dell2g.1996@gmail.com>
2025-11-24 10:18:18 +0900

    원복

--- Source/GAS_Project/Components/CWeaponComponent.cpp
+++ Source/GAS_Project/Components/CWeaponComponent.cpp
@@ -1,191 +1,37 @@
 #include "CWeaponComponent.h"
 
-#include "GAS_Project/Characters/CCharacter.h"
 #include "GAS_Project/Item/Weapon/CWeapon.h"
-#include "Net/UnrealNetwork.h"
 
-
-UCWeaponComponent::UCWeaponComponent()
-{
-	PrimaryComponentTick.bCanEverTick = false;
-	SetIsReplicatedByDefault(true);
-}
-
-void UCWeaponComponent::BeginPlay()
-{
-	Super::BeginPlay();
-	CacheOwnerMesh();
-
-}
-
-void UCWeaponComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
-{
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-    DOREPLIFETIME(UCWeaponComponent, CurrentEquippedWeaponTag);
-}
-
-void UCWeaponComponent::OnRep_CurrentEquippedWeaponTag()
-{
-}
-
-ACWeapon* UCWeaponComponent::GetWeaponByTag(FGameplayTag WeaponTag) const
-{
-	if (ACWeapon* const* FoundWeapon = WeaponMap.Find(WeaponTag))
-	{
-		return *FoundWeapon;
-	}
-	return nullptr;
-}
-
-class ACWeapon* UCWeaponComponent::GetCurrentWeapon() const
-{
-	return GetWeaponByTag(CurrentEquippedWeaponTag);
-}
-
-void UCWeaponComponent::OnRep_CurrentWeapon(class ACWeapon* OldWeapon)
-{
-	// 클라이언트에서 무기 변경 시 시각적 업데이트
-	UE_LOG(LogTemp, Log, TEXT("OnRep_CurrentWeapon: Old=%s, New=%s"), 
-		OldWeapon ? *OldWeapon->GetName() : TEXT("None"),
-		CurrentWeapon ? *CurrentWeapon->GetName() : TEXT("None"));
-
-	// UI 업데이트, 사운드 재생 등 클라이언트 측 피드백
-	// 예: 무기 교체 사운드, UI 갱신
-}
-
-void UCWeaponComponent::AttachWeapon(class ACWeapon* NewWeapon)
-{
-	// 서버에서만 실행되어야 함 (Ability에서 서버 권한 보장)
-	if (GetOwnerRole() != ROLE_Authority)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("AttachWeapon called on non-authority"));
-		return;
-	}
-
-	if (!NewWeapon)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("AttachWeapon: NewWeapon is null"));
-		return;
-	}
-
-	// 같은 무기면 스킵
-	if (NewWeapon == CurrentWeapon)
-	{
-		return;
-	}
-
-	// 기존 무기가 있으면 먼저 해제
-	if (CurrentWeapon)
-	{
-		DetachWeapon();
-	}
-
-	// 새 무기 설정
-	CurrentWeapon = NewWeapon;
-	CurrentWeapon->SetOwner(GetOwner());
-
-	CacheOwnerMesh();
-
-	if (OwnerMesh)
-	{
-		CurrentWeapon->AttachToComponent(
-			OwnerMesh,
-			FAttachmentTransformRules::SnapToTargetIncludingScale,
-			TEXT("WeaponSocket") // 소켓 이름은 프로젝트에 맞게 수정
-		);
-
-		UE_LOG(LogTemp, Log, TEXT("Weapon attached to socket on %s"), *GetOwner()->GetName());
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("OwnerMesh not found for weapon attachment"));
-	}
-}
-
-void UCWeaponComponent::DetachWeapon()
-{
-	// 서버에서만 실행
-	if (GetOwnerRole() != ROLE_Authority)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("DetachWeapon called on non-authority"));
-		return;
-	}
-
-	if (!CurrentWeapon)
-	{
-		return;
-	}
-
-	CurrentWeapon->DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
-	CurrentWeapon->SetOwner(nullptr);
-    
-	// 필요시 무기를 파괴하거나 인벤토리로 반환
-	// CurrentWeapon->Destroy();
-    
-	CurrentWeapon = nullptr;
-
-	UE_LOG(LogTemp, Log, TEXT("Weapon detached from %s"), *GetOwner()->GetName());
-}
-
-void UCWeaponComponent::CacheOwnerMesh()
-{
-	if (!OwnerMesh)
-	{
-		if (ACharacter* OwnerCharacter = Cast<ACCharacter>(GetOwner()))
-		{
-			OwnerMesh = OwnerCharacter->GetMesh();
-		}
-	}
-}
 
 void UCWeaponComponent::RegisterSpawnedWeapon(struct FGameplayTag InWeaponTag, class ACWeapon* InWeapon, bool bRegister)
 {
-	// WeaponMap.Emplace(InWeaponTag,InWeapon);
-	//
-	// if (bRegister)
-	// {
-	// 	CurrentEquippedWeaponTag = InWeaponTag;
-	// }
-
-
-	if (!InWeaponTag.IsValid() || !InWeapon)
-	{
-		return;
-	}
-
+	WeaponMap.Emplace(InWeaponTag,InWeapon);
+	
 	if (bRegister)
 	{
-		WeaponMap.Add(InWeaponTag, InWeapon);
-		UE_LOG(LogTemp, Log, TEXT("RegisterSpawnedWeapon: Registered %s"), *InWeaponTag.ToString());
-	}
-	else
-	{
-		WeaponMap.Remove(InWeaponTag);
-		UE_LOG(LogTemp, Log, TEXT("RegisterSpawnedWeapon: Unregistered %s"), *InWeaponTag.ToString());
+		CurrentEquippedWeaponTag = InWeaponTag;
 	}
 }
 
-// ACWeapon* UCWeaponComponent::GetCarriedWeaponByTag(FGameplayTag InWeaponTagToGet) const
-// {
-// 	// if (WeaponMap.Contains(InWeaponTagToGet))
-// 	// {
-// 	// 	if (ACWeapon* const* FoundWeapon = WeaponMap.Find(InWeaponTagToGet))
-// 	// 	{
-// 	// 		return *FoundWeapon;
-// 	// 	}
-// 	// }
-// 	//
-// 	// return nullptr;
-//
-// 	
-// }
+ACWeapon* UCWeaponComponent::GetCarriedWeaponByTag(FGameplayTag InWeaponTagToGet) const
+{
+	if (WeaponMap.Contains(InWeaponTagToGet))
+	{
+		if (ACWeapon* const* FoundWeapon = WeaponMap.Find(InWeaponTagToGet))
+		{
+			return *FoundWeapon;
+		}
+	}
 
-// ACWeapon* UCWeaponComponent::GetCharacterCurrentEquippedWeapon() const
-// {
-// 	if (!CurrentEquippedWeaponTag.IsValid())
-// 	{
-// 		return nullptr;
-// 	}
-//  
-// 	return GetCarriedWeaponByTag(CurrentEquippedWeaponTag);
-// }
+	return nullptr;
+}
+
+ACWeapon* UCWeaponComponent::GetCharacterCurrentEquippedWeapon() const
+{
+	if (!CurrentEquippedWeaponTag.IsValid())
+	{
+		return nullptr;
+	}
+ 
+	return GetCarriedWeaponByTag(CurrentEquippedWeaponTag);
+}
--- Source/GAS_Project/Components/CWeaponComponent.h
+++ Source/GAS_Project/Components/CWeaponComponent.h
@@ -12,56 +12,19 @@ UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class GAS_PROJECT_API UCWeaponComponent : public UActorComponent
 {
 	GENERATED_BODY()
-public:
-	UCWeaponComponent();
-protected:
-	virtual void BeginPlay() override;
-protected:
-	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-	UFUNCTION()
-	void OnRep_CurrentEquippedWeaponTag();
+public:	
+	UFUNCTION(BlueprintCallable, Category="Comp")
+	void RegisterSpawnedWeapon(struct FGameplayTag InWeaponTag, class ACWeapon* InWeapon , bool bRegister = false);
 
-	// 복제되는 현재 무기 (서버 권한)
-	UPROPERTY(ReplicatedUsing=OnRep_CurrentWeapon, BlueprintReadOnly, Category="Weapon")
-	class ACWeapon* CurrentWeapon;
+	UFUNCTION(BlueprintCallable, Category = "Warrior|Combat")
+	ACWeapon* GetCarriedWeaponByTag(FGameplayTag InWeaponTagToGet) const;
 
-	UFUNCTION()
-	void OnRep_CurrentWeapon(class ACWeapon* OldWeapon);
-
-public:
-	// Ability에서 호출할 함수들
-	UFUNCTION(BlueprintCallable, Category="Weapon")
-	void AttachWeapon(class ACWeapon* NewWeapon);
-
-	UFUNCTION(BlueprintCallable, Category="Weapon")
-	void DetachWeapon();
-
-	UFUNCTION(BlueprintPure, Category="Weapon")
-	bool HasWeaponEquipped() const { return CurrentWeapon != nullptr; }
-
-	//Legacy Code
-	// 스폰된 무기 등록
-	UFUNCTION(BlueprintCallable, Category="Weapon")
-	void RegisterSpawnedWeapon(FGameplayTag InWeaponTag, ACWeapon* InWeapon, bool bRegister);
-
-	// 무기 가져오기 (태그로)
-	UFUNCTION(BlueprintPure, Category="Weapon")
-	ACWeapon* GetWeaponByTag(FGameplayTag WeaponTag) const;
-
-	// 현재 장착된 무기 태그
-	UPROPERTY(ReplicatedUsing=OnRep_CurrentEquippedWeaponTag, BlueprintReadWrite, Category="Weapon")
+	UPROPERTY(BlueprintReadWrite, Category = "Warrior|Combat")
 	FGameplayTag CurrentEquippedWeaponTag;
 
-	// 현재 장착된 무기 가져오기
-	UFUNCTION(BlueprintPure, Category="GAS|Weapon")
-	class ACWeapon* GetCurrentWeapon() const;
-
-private:
-	UPROPERTY()
-	USkeletalMeshComponent* OwnerMesh;
-
-	void CacheOwnerMesh();
+	UFUNCTION(BlueprintCallable, Category = "Warrior|Combat")
+	ACWeapon* GetCharacterCurrentEquippedWeapon() const;
 	
 private:
 	TMap<struct FGameplayTag, class ACWeapon*> WeaponMap;
