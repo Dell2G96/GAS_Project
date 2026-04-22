9dfffe2f822617557e2eb124fba9a4fa90f3c3a6
Dell2G96 <dell2g.1996@gmail.com>
2026-04-22 21:11:34 +0900

    public

--- Plugins/ModularGamePlayActors/Source/ModularGamePlayActors/Public/ModularPlayerController.h
+++ Plugins/ModularGamePlayActors/Source/ModularGamePlayActors/Public/ModularPlayerController.h
@@ -13,7 +13,7 @@ UCLASS()
 class MODULARGAMEPLAYACTORS_API AModularPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-
+public:
 	virtual void PreInitializeComponents() override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 };
